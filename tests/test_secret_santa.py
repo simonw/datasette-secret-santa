cef3aed7afa523dc07190e7d735e46ecd61e4f5e
Simon Willison <swillison@gmail.com>
2022-12-10 13:15:27 -0800

    Test covering the entire flow, closes #2

--- tests/test_secret_santa.py
+++ tests/test_secret_santa.py
@@ -28,3 +28,106 @@ async def test_error_on_startup_if_no_santa_database():
     with pytest.raises(AssertionError) as ex:
         await ds.invoke_startup()
         assert ex.value == "datasette-secret-santa plugin requires santa.db database"
+
+
+@pytest.mark.asyncio
+async def test_full_flow(ds):
+    db = ds.get_database("santa")
+    tables = await db.table_names()
+    assert tables == ["secret_santa", "secret_santa_participants"]
+    assert (await ds.client.get("/secret-santa/demo")).status_code == 404
+    await db.execute_write(
+        "insert into secret_santa (slug, name) values (?, ?)",
+        ["demo", "Demo secret santa"],
+    )
+    response1 = await ds.client.get("/secret-santa/demo")
+    assert response1.status_code == 200
+    assert "Demo secret santa" in response1.text
+    csrftoken = response1.cookies["ds_csrftoken"]
+
+    # Now add some participants
+    for i in range(1, 5):
+        post_response = await ds.client.post(
+            "/secret-santa/demo/add",
+            data={"name": f"person-{i}", "csrftoken": csrftoken},
+        )
+        assert post_response.status_code == 302
+        assert post_response.headers["location"] == "/secret-santa/demo"
+
+    assign_button = '<input type="submit" value="Assign recipients!">'
+
+    # Participants should now be listed
+    response2 = await ds.client.get("/secret-santa/demo")
+    assert response2.status_code == 200
+    for i in range(1, 5):
+        assert f"person-{i}" in response2.text
+    assert assign_button not in response2.text
+
+    # But should not have public/private keys
+    for row in await db.execute("select * from secret_santa_participants"):
+        assert not row["public_key"]
+        assert not row["private_key_encrypted_iv"]
+        assert not row["private_key_encrypted_ct"]
+
+    # Each user now sets their password
+    participant_passwords = {}
+    for row in await db.execute("select * from secret_santa_participants"):
+        id = row["id"]
+        response3 = await ds.client.get(f"/secret-santa/demo/set-password/{id}")
+        assert response3.status_code == 200
+        assert f"click this button if you are {row['name']}" in response3.text
+        set_password_response = await ds.client.post(
+            f"/secret-santa/demo/set-password/{id}",
+            data={"csrftoken": csrftoken},
+        )
+        assert set_password_response.status_code == 200
+        assert "Your secret password is" in set_password_response.text
+        password = set_password_response.text.split(' class="your-password">')[1].split(
+            "<"
+        )[0]
+        participant_passwords[id] = (row["name"], password)
+
+    # OK, at this point everyone should have a public/private key in the DB
+    for row in await db.execute("select * from secret_santa_participants"):
+        assert row["public_key"]
+        assert row["private_key_encrypted_iv"]
+        assert row["private_key_encrypted_ct"]
+        # But not a message
+        assert not row["secret_message_encrypted"]
+
+    # Now we can generate the secret santa
+    response4 = await ds.client.get("/secret-santa/demo")
+    assert assign_button in response4.text
+
+    # Click that assign button
+    assign_response = await ds.client.post(
+        "/secret-santa/demo/assign",
+        data={"csrftoken": csrftoken},
+    )
+    assert assign_response.status_code == 302
+    assert assign_response.headers["location"] == "/secret-santa/demo"
+
+    # Now everyone should have a message
+    for row in await db.execute("select * from secret_santa_participants"):
+        assert row["secret_message_encrypted"]
+        assert not row["message_read_at"]
+
+    # Use their passwords to decrypt their messages
+    recipients = set()
+    for id, (name, password) in participant_passwords.items():
+        decrypt_response = await ds.client.post(
+            f"/secret-santa/demo/reveal/{id}",
+            data={"password": password, "csrftoken": csrftoken},
+        )
+        assert decrypt_response.status_code == 200
+        secret_message = decrypt_response.text.split(' class="secret-message">')[
+            1
+        ].split("<")[0]
+        assert secret_message.startswith("You should buy a gift for ")
+        recipient = secret_message.split()[-1]
+        # Should not be buying a gift for themselves
+        assert name != recipient
+        recipients.add(recipient)
+
+    # Everyone should be a recipient
+    assert len(recipients) == len(participant_passwords)
