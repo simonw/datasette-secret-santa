6d7c6c61b91d9e8674726fc5a1c3427a60316ce7
Simon Willison <swillison@gmail.com>
2022-12-09 19:24:57 -0800

    Initial structure from simonw/datasette-plugin

---
+++ README.md
@@ -0,0 +1,34 @@
+# datasette-secret-santa
+
+[![PyPI](https://img.shields.io/pypi/v/datasette-secret-santa.svg)](https://pypi.org/project/datasette-secret-santa/)
+[![Changelog](https://img.shields.io/github/v/release/simonw/datasette-secret-santa?include_prereleases&label=changelog)](https://github.com/simonw/datasette-secret-santa/releases)
+[![Tests](https://github.com/simonw/datasette-secret-santa/workflows/Test/badge.svg)](https://github.com/simonw/datasette-secret-santa/actions?query=workflow%3ATest)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://github.com/simonw/datasette-secret-santa/blob/main/LICENSE)
+
+Run a secret santa using Datasette
+
+## Installation
+
+Install this plugin in the same environment as Datasette.
+
+    datasette install datasette-secret-santa
+
+## Usage
+
+Usage instructions go here.
+
+## Development
+
+To set up this plugin locally, first checkout the code. Then create a new virtual environment:
+
+    cd datasette-secret-santa
+    python3 -m venv venv
+    source venv/bin/activate
+
+Now install the dependencies and test dependencies:
+
+    pip install -e '.[test]'
+
+To run the tests:
+
+    pytest
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+import os
+
+VERSION = "0.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="datasette-secret-santa",
+    description="Run a secret santa using Datasette",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Simon Willison",
+    url="https://github.com/simonw/datasette-secret-santa",
+    project_urls={
+        "Issues": "https://github.com/simonw/datasette-secret-santa/issues",
+        "CI": "https://github.com/simonw/datasette-secret-santa/actions",
+        "Changelog": "https://github.com/simonw/datasette-secret-santa/releases",
+    },
+    license="Apache License, Version 2.0",
+    classifiers=[
+        "Framework :: Datasette",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+    version=VERSION,
+    packages=["datasette_secret_santa"],
+    entry_points={"datasette": ["secret_santa = datasette_secret_santa"]},
+    install_requires=["datasette"],
+    extras_require={"test": ["pytest", "pytest-asyncio"]},
+    package_data={
+        "datasette_secret_santa": ["static/*", "templates/*"]
+    },
+    python_requires=">=3.7",
+)
